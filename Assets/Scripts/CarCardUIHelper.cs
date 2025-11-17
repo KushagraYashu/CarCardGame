cbe5c2f7222c747da321a42b6dcfcbb5775220bd
Kushagra <syashu31+github@gmail.com>
2025-11-17 18:23:40 +0000

    added icon and mask, started UI fetching and placing

---
+++ Assets/Scripts/CarCardUIHelper.cs
@@ -0,0 +1,27 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class CarCardUIHelper : MonoBehaviour
+{
+    [Serializable]
+    public class CarCardUIObj
+    {
+        public TextMeshProUGUI title;
+        public TextMeshProUGUI value;
+    }
+
+    public CarCardUIObj paceRatingUI;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
